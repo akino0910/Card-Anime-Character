0a7022c2f9547d4e4749766bd819cc4262a25b25
Thái Minh Sang <sang024854@gmail.com>
2018-05-05 16:54:49 +0700

    update gitignore, remove file never use

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Layout from './layouts/Layout';
-import registerServiceWorker from './registerServiceWorker';
 
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
@@ -19,5 +18,3 @@ ReactDOM.render(
 	</Provider>,
 	document.getElementById('root')
 );
-
-registerServiceWorker();
